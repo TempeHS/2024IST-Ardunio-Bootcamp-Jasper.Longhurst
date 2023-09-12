ebbdc389a5197dfed0755b39c11bbc26e3620e14
Swebwite <jasper.longhurst@education.nsw.gov.au>
2023-09-12 10:33:36 +1000

    update

---
+++ 13.I2C/functions.ino
@@ -0,0 +1,12 @@
+void updateOLED (float theTemp) {
+  u8g2.clearBuffer();                  // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+  u8g2.setCursor(0, 10);              // Set cursor to first line
+  u8g2.print("Hello World!");         // write something to the internal memory
+  u8g2.setCursor(0, 20);
+  u8g2.print("The temparature is: ");
+  u8g2.setCursor(0, 30);
+  u8g2.print(theTemp);
+  u8g2.print("°C");
+  u8g2.sendBuffer();                  // transfer internal memory to the display
+}
